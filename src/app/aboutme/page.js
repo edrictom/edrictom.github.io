bee06831283fee16f56bb19123fa533ff0b85aba
Edric Tom <edrictom@icloud.com>
2023-05-19 16:51:00 -0700

    Begin About Me nav implementation

--- src/app/aboutme/page.js
+++ src/app/aboutme/page.js
@@ -1,19 +1,42 @@
-import Header from '../../components/Header/header';
+import Profile from '@/components/Header/Profile/profile';
 import Image from 'next/image';
+import Link from 'next/link';
+
+const navSections = ["Profile", "Hobbies", "Contact"];
+let currentSelection = "Profile";
 
 export default function AboutMe() {
     return (
-        <main className="flex min-h-screen flex-col items-center p-8">
-            <div className="flex flex-row justify-start w-full">
-                <Image
-                    src="/Icons/exit.svg"
-                    alt="X icon to go back home"
-                    className="dark:invert mr-6"
-                    width={ 50 }
-                    height={ 24 }
-                />
-                <Header />
-            </div>
+        <main className="flex min-h-screen flex-row m-8">
+            <nav className="flex flex-col justify-start border-r-2 border-black  pr-24">
+                <div className="flex flex-row justify-start w-full">
+                    <Link href="/">
+                        <Image
+                            src="/Icons/exit.svg"
+                            alt="X icon to go back home"
+                            className="dark:invert m-2 mr-7"
+                            width={ 50 }
+                            height={ 24 }
+                        />
+                    </Link>
+                    <Profile />
+                </div>
+                <section className="mt-36 flex flex-col">
+                    { navSections.map(section => {
+                        return (
+                            <button className="text-center text-xl my-3 py-6 font-bold hover:outline hover:outline-black">
+                                <input
+                                    className="hidden"
+                                    type="radio"
+                                    id={ section }
+                                    name="navSelector"
+                                />
+                                <label className="" for={section}>{ section }</label>
+                            </button>
+                        );
+                    }) }
+                </section>
+            </nav>
         </main>
     );
 }
--- src/components/Header/Profile/profile.js
+++ src/components/Header/Profile/profile.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Profile() {
     return (
@@ -12,22 +11,20 @@ export default function Profile() {
                 height={ 24 }
                 priority
             />
-            <Link href="/">
-                <div className="flex flex-col p-2">
-                    <p className="whitespace-nowrap">Edric Tom</p>
-                    <div className="flex flex-row">
-                        <Image
-                            src="/Icons/fire.svg"
-                            alt="Fire icon"
-                            className="dark:invert px-1"
-                            width={ 25 }
-                            height={ 35 }
-                            priority
-                        />
-                        <span>54250</span>
-                    </div>
+            <div className="flex flex-col p-4">
+                <p className="whitespace-nowrap">Edric Tom</p>
+                <div className="flex flex-row">
+                    <Image
+                        src="/Icons/fire.svg"
+                        alt="Fire icon"
+                        className="dark:invert px-1"
+                        width={ 25 }
+                        height={ 35 }
+                        priority
+                    />
+                    <span>54250</span>
                 </div>
-            </Link>
+            </div>
         </div>
     );
 }
