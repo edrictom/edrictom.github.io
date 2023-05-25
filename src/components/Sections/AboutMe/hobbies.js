efbe5197abfd64d250e7fb0e70ca781667a5ac72
Edric Tom <edrictom@icloud.com>
2023-05-25 09:29:49 -0700

    Test commit from work PC

--- src/components/Sections/AboutMe/hobbies.js
+++ src/components/Sections/AboutMe/hobbies.js
@@ -3,6 +3,7 @@ export default function Hobbies() {
     return (
         <div className="flex flex-col space-y-5 justify-center items-center">
             <h1 className="text-4xl font-bold p-24">When I am not coding, I am...</h1>
+            <h2 className="text-2xl font-semibold p-20">Rock Climbing!</h2>
         </div>
     );
 }
