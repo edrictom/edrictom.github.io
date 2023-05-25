ed58f3bbb8193d371b99fb53ee7de16ef540f20b
Edric Tom <edrictom@icloud.com>
2023-05-25 00:19:10 -0700

    Add some content to DevTools, test signed commit

--- src/components/Sections/Skills/devtools.js
+++ src/components/Sections/Skills/devtools.js
@@ -5,12 +5,22 @@ export default function DevTools() {
             <h1 className="p-24 text-4xl font-extrabold">
                 Frameworks
             </h1>
+            <ul className="flex flex-row justify-evenly w-full">
+                <li>React</li>
+                <li>Tailwind CSS</li>
+                <li>Express</li>
+                <li>Next.js</li>
+            </ul>
             <h1 className="p-24 text-4xl font-extrabold">
                 Libraries
             </h1>
             <h1 className="p-24 text-4xl font-extrabold">
                 IDEs
             </h1>
+            <ul className="flex flex-row justify-evenly w-full">
+                <li>Xcode</li>
+                <li>Visual Studio Code</li>
+            </ul>
         </div>
     );
 }
