14b1687a4105197edc68fc93d15ab2035ed85a9b
Edric Tom <edrictom@icloud.com>
2023-05-25 14:24:31 -0700

    Add Font Awesome packages to replace SVG files

--- src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
+++ src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
@@ -1,40 +1,35 @@
 import SocialsTile from '../SocialsTile/socialsTile';
-import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 const socialsData = [
     {
         title: 'GitHub',
-        image: <Image
-            src={ '/Icons/github.svg' }
-            alt={ "GitHub icon" }
-            width={ 0 }
-            height={ 0 }
-            className="w-16 h-16 self-center"
-        />,
+        image: <FontAwesomeIcon
+                icon={faGithub}
+                size="4x"
+                className="text-black self-center"
+                />,
         link: 'https://www.github.com/edrictom',
         key: 2
     },
     {
         title: 'LinkedIn',
-        image: <Image
-            src={ '/Icons/linkedin.svg' }
-            alt={ "LinkedIn icon" }
-            width={ 0 }
-            height={ 0 }
-            className="w-16 h-16 self-center"
-        />,
+        image: <FontAwesomeIcon
+                icon={faLinkedin}
+                size="4x"
+                className="text-black self-center"
+                />,
         link: 'https://www.linkedin.com/in/edric-tom-98ba34178',
         key: 3
     },
     {
         title: 'YouTube',
-        image: <Image
-            src={ '/Icons/youtube.svg' }
-            alt={ " Youtube icon" }
-            width={ 0 }
-            height={ 0 }
-            className="w-16 h-16 self-center"
-        />,
+        image: <FontAwesomeIcon
+                icon={faYoutube}
+                size="4x"
+                className="text-black self-center"
+                />,
         link: 'https://youtube.com/@EdricTom',
         key: 4
     }
