61983b46403cecf5ddcc54f9f86646d5980c7592
Edric Tom <edrictom@icloud.com>
2023-05-31 16:51:03 -0700

    More responsive design implementation

--- src/components/MainMenu/TileList/tileList.js
+++ src/components/MainMenu/TileList/tileList.js
@@ -10,7 +10,7 @@ const tileData = [
         image: <UserCircleIcon className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 2xl:h-24 2xl:w-24 text-black self-center" />,
         link: '/aboutme',
         height: "h-24 md:h-32 lg:h-48 2xl:h-96",
-        width: "w-24 md:w-32 lg:w-48 2xl:w-96 2xl:m-6 mb-6",
+        width: "w-24 md:w-32 lg:w-48 2xl:w-96 2xl:m-6 md:mb-6",
         key: 1
     },
     {
@@ -18,7 +18,7 @@ const tileData = [
         image: <CodeBracketIcon className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 2xl:h-24 2xl:w-24 text-black self-center" />,
         link: '/skills',
         height: "h-24 md:h-32 lg:h-48 2xl:h-64",
-        width: "w-24 md:w-32 lg:w-48 2xl:w-64 mb-6",
+        width: "w-24 md:w-32 lg:w-48 2xl:w-64 md:mb-6",
         key: 2
     },
     { 
@@ -26,7 +26,7 @@ const tileData = [
         image: <BriefcaseIcon className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 2xl:h-24 2xl:w-24 text-black self-center" />, 
         link: '/experience',
         height: "h-24 md:h-32 lg:h-48 2xl:h-64",
-        width: "w-24 md:w-32 lg:w-48 2xl:w-64 mb-6",
+        width: "w-24 md:w-32 lg:w-48 2xl:w-64 md:mb-6",
         key: 3 
     },
     { 
@@ -37,7 +37,7 @@ const tileData = [
                 />, 
         link: '/medals',
         height: "h-24 md:h-32 lg:h-48 2xl:h-64",
-        width: "w-24 md:w-32 lg:w-48 2xl:w-64 mb-6",
+        width: "w-24 md:w-32 lg:w-48 2xl:w-64 md:mb-6",
         key: 4 
     },
     { 
@@ -45,14 +45,14 @@ const tileData = [
         image: <Cog8ToothIcon className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 2xl:h-24 2xl:w-24 text-black self-center" />,
         link: '/settings',
         height: "h-24 md:h-32 lg:h-48 2xl:h-64",
-        width: "w-24 md:w-32 lg:w-48 2xl:w-64 mb-6",
+        width: "w-24 md:w-32 lg:w-48 2xl:w-64 md:mb-6",
         key: 5 
     }
 ];
 
 export default function TileList() {
     return (
-        <ul className="flex flex-row flex-wrap xl:space-x-3 justify-evenly items-end">
+        <ul className="grid grid-cols-2 max-md:gap-2 md:flex md:flex-row md:flex-wrap md:max-xl:gap-x-2 xl:space-x-3 justify-evenly items-end">
             { tileData.map(tile => {
                 return <Tile height={ tile.height } width={ tile.width } title={ tile.title } image={ tile.image } link={ tile.link } key={ tile.key } />
             }) }
--- src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
+++ src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
@@ -10,7 +10,7 @@ const socialsData = [
         image: <WindowIcon className="h-10 w-10 xl:h-20 xl:w-20 text-black self-center" />,
         link: '/projects',
         height: "h-24 xl:h-32 2xl:h-64",
-        width: "w-32 xl:w-48 2xl:w-96 2xl:mr-10 mb-6",
+        width: "w-32 xl:w-48 2xl:w-96 2xl:mr-10 md:mb-6",
         key: 1
     },
     {
@@ -21,7 +21,7 @@ const socialsData = [
                 />,
         link: 'https://www.github.com/edrictom',
         height: "h-24 xl:h-32 2xl:h-48",
-        width: "w-32 xl:w-48 2xl:w-80 mb-6",
+        width: "w-32 xl:w-48 2xl:w-80 md:mb-6",
         key: 2
     },
     {
@@ -32,7 +32,7 @@ const socialsData = [
                 />,
         link: 'https://www.linkedin.com/in/edric-tom-98ba34178',
         height: "h-24 xl:h-32 2xl:h-48",
-        width: "w-32 xl:w-48 2xl:w-80 mb-6",
+        width: "w-32 xl:w-48 2xl:w-80 md:mb-6",
         key: 3
     },
     {
@@ -43,14 +43,14 @@ const socialsData = [
                 />,
         link: 'https://youtube.com/@EdricTom',
         height: "h-24 xl:h-32 2xl:h-48",
-        width: "w-32 xl:w-48 2xl:w-80 mb-6",
+        width: "w-32 xl:w-48 2xl:w-80 md:mb-6",
         key: 4
     }
 ];
 
 export default function SocialTileList() {
     return (
-        <ul className="flex flex-row flex-wrap xl:space-x-3 justify-evenly items-end">
+        <ul className="grid grid-cols-2 max-md:gap-4 md:flex md:flex-row md:flex-wrap md:max-xl:gap-x-2 xl:space-x-3 justify-evenly items-end">
             { socialsData.map(tile => {
                 return <SocialsTile
                     height= { tile.height }
