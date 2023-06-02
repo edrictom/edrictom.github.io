ed370e716f6b0dc7323be4aa33b99031e6f72396
Edric Tom <edrictom@icloud.com>
2023-06-02 15:42:31 -0700

    FIx group hover on secondary menu

--- src/components/SecondaryMenu/SocialsTile/socialsTile.js
+++ src/components/SecondaryMenu/SocialsTile/socialsTile.js
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 
-export default function SocialsTile({ height, width, title, altTitle, image, link }) {
+export default function SocialsTile({ title, altTitle, style, image, link }) {
     return (
         <Link href={ link }>
-            <div className={ "group relative shadow-2xl bg-gray-500 " + height + " " + width + " flex flex-col justify-center items-center rounded-md outline outline-4 outline-transparent hover-outline-color transition-all duration-75" }>
+            <div className={ "relative shadow-2xl bg-gray-500 " + style + " flex flex-col justify-center items-center rounded-md outline outline-4 outline-transparent hover-outline-color transition-all duration-75" }>
                 <div className="flex-1 flex pb-2 md:p-0">
                     { image }
                 </div>
-                <p className="hidden lg:block absolute bottom-0 group-hover:block w-full pb-2 md:p-3 xl:p-4 font-semibold text-black text-xs lg:text-sm 2xl:text-lg text-center md:text-left">
+                <p className="hidden lg:block absolute bottom-0 w-full pb-2 md:p-3 xl:p-4 font-semibold text-black text-xs lg:text-sm 2xl:text-lg text-center md:text-left">
                     { title }
                 </p>
-                <p className="lg:hidden absolute bottom-0 group-hover:block w-full pb-2 md:p-3 xl:p-4 font-semibold text-black text-xs lg:text-sm 2xl:text-lg text-center md:text-left">
+                <p className="lg:hidden absolute bottom-0 w-full pb-2 md:p-3 xl:p-4 font-semibold text-black text-xs lg:text-sm 2xl:text-lg text-center md:text-left">
                     { altTitle ? altTitle : title }
                 </p>
             </div>
--- src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
+++ src/components/SecondaryMenu/SocialsTileList/socialsTileList.js
@@ -10,8 +10,7 @@ const socialsData = [
         altTitle: 'Projects',
         image: <WindowIcon className="h-10 w-10 lg:h-16 lg:w-16 2xl:h-20 2xl:w-20 text-black self-center" />,
         link: '/projects',
-        height: "h-24 md:h-28 lg:h-40 2xl:h-64",
-        width: "w-32 md:w-40 lg:w-60 2xl:w-96 2xl:mr-10 md:mb-6",
+        style: "h-24 md:h-28 lg:h-40 2xl:h-64 w-32 md:w-40 lg:w-60 2xl:w-96 2xl:mr-10 md:mb-6",
         key: 1
     },
     {
@@ -21,8 +20,7 @@ const socialsData = [
                 className="text-black text-4xl lg:text-5xl 2xl:text-6xl self-center"
                 />,
         link: 'https://www.github.com/edrictom',
-        height: "h-24 md:h-28 lg:h-40 2xl:h-48",
-        width: "w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
+        style: "h-24 md:h-28 lg:h-40 2xl:h-48 w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
         key: 2
     },
     {
@@ -32,8 +30,7 @@ const socialsData = [
                 className="text-black text-4xl lg:text-5xl 2xl:text-6xl self-center"
                 />,
         link: 'https://www.linkedin.com/in/edric-tom-98ba34178',
-        height: "h-24 md:h-28 lg:h-40 2xl:h-48",
-        width: "w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
+        style: "h-24 md:h-28 lg:h-40 2xl:h-48 w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
         key: 3
     },
     {
@@ -43,8 +40,7 @@ const socialsData = [
                 className="text-black text-4xl lg:text-5xl 2xl:text-6xl self-center"
                 />,
         link: 'https://youtube.com/@EdricTom',
-        height: "h-24 md:h-28 lg:h-40 2xl:h-48",
-        width: "w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
+        style: "h-24 md:h-28 lg:h-40 2xl:h-48 w-32 md:w-40 lg:w-60 2xl:w-80 md:mb-6",
         key: 4
     }
 ];
@@ -54,11 +50,10 @@ export default function SocialTileList() {
         <ul className="grid grid-cols-2 max-md:gap-4 md:flex md:flex-row md:flex-wrap md:max-xl:gap-x-2 xl:space-x-3 justify-evenly items-end">
             { socialsData.map(tile => {
                 return <SocialsTile
-                    height= { tile.height }
-                    width={ tile.width }
                     title={ tile.title }
                     altTitle={ tile.altTitle }
                     image={ tile.image }
+                    style= { tile.style }
                     link={ tile.link }
                     key={ tile.key }
                 />
